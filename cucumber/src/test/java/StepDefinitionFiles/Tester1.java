305854c808dd9cbe6149e4a6bb0588dfa1ae31f5
santoshss24 <66872766+santoshss24@users.noreply.github.com>
2020-12-02 17:44:43 +0530

    Added testcase Tester1

---
+++ cucumber/src/test/java/StepDefinitionFiles/Tester1.java
@@ -0,0 +1,16 @@
+package StepDefinitionFiles;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import org.openqa.selenium.WebDriver;
+
+public class Tester1 
+{
+	public static void main(String[] args) {
+		
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		
+	}
+
+}
